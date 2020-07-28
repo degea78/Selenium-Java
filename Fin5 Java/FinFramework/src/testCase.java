8a8d8136870ed4b841e81da5aec276232ef1a2c2
Bogdan Eugen <53865191+degea78@users.noreply.github.com>
2020-07-28 17:36:56 +0300

    Add files via upload
    first upload

---
+++ Fin5 Java/FinFramework/src/testCase.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testCase {
+
+	public static void main(String[] args) {		
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.ro/");
+		String title = driver.getTitle();
+
+		System.out.println(title);
+		
+		
+	}
+
+}
